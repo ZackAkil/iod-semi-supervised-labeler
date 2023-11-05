4a564b5c665c8381e9862363e50317fcb9788798
Zack Akil <8178508+ZackAkil@users.noreply.github.com>
2023-11-05 14:29:42 +0000

    uploaidng files to stroage

---
+++ firebase.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+
+import { getStorage, ref, uploadString } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-storage.js";
+
+
+const storage = getStorage(firebaseApp, BUCKET);
+
+
+export function upload_file(name, dataUrl, bboxes = '') {
+
+    const storageRef = ref(storage, name);
+
+    const metadata = {
+        contentType: 'image/png',
+        customMetadata: {
+            'bboxes': bboxes
+        }
+    };
+
+    uploadString(storageRef, dataUrl, 'data_url', metadata).then((snapshot) => {
+        console.log('Uploaded a data_url string! ✅ :', name);
+    });
+
+}
+
+// const name = 'hi'
+// const dataUrl = 'data:text/plain;base64,5b6p5Y+344GX44G+44GX44Gf77yB44GK44KB44Gn44Go44GG77yB'
+// const bbox = 'hello'
+// upload_file(name, dataUrl, bbox)
